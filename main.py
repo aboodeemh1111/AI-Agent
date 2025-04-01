cba8c3183a3ad64fabbbb0040c91d132ec026564
aboodeemh1111 <62642467+aboodeemh1111@users.noreply.github.com>
2025-04-01 09:39:40 +0300

    finished the basic program

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool,wiki_tool,save_tool
 
 load_dotenv()
 
@@ -43,22 +44,27 @@ prompt = ChatPromptTemplate.from_messages([
 ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+
+tools = [search_tool,wiki_tool]
+
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
     
 )
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("Enter your query: ")
 raw_response = agent_executor.invoke({
-    "query": "what tech stack was used to build you",
+    "query": query,
     "chat_history": []
 })
 
 try:
     structured_response = parser.parse(raw_response.get("output")[0]["text"])
 except Exception as e:
-    print("Error parsing response:", e,"\nRaw response:", raw_response )
-    
+    print(f"Error parsing response: {e}")
+    structured_response = None
 
-print(structured_response)
+if structured_response:
+    print(structured_response)
